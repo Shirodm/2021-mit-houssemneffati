4b464279734f9cc838f418c5ac61bbca021ea0f3
Shirodm <houssem.neffati@outlook.com>
2021-02-09 12:57:17 +0300

    Added junior part

---
+++ docker.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+echo "--- Hello ---"
+
+echo "--- Updating packages list ---"
+sudo apt-get -qq update
+sudo apt-get -qq install curl
+echo "--- Installing docker ---"
+sudo apt-get -y -qq install docker.io
+echo "--- Installing docker-compose ---"
+sudo curl -s -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo service docker start
+sudo chmod +x /usr/local/bin/docker-compose
+sudo groupadd docker
+sudo usermod -aG docker vagrant
+sudo docker --version
+sudo docker-compose --version
+sudo docker run hello-world
+
+
+
+echo "---Task done !---"
